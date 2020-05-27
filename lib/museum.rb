a85c0a0b1ce216aa618f5d1eadbcdf410b3b6bc0
Kiera Allen <kieraaallen@gmail.com>
2020-05-27 09:41:07 -0600

    Add recommend_exhibits method and test

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,16 @@ class Museum
   def add_exhibit(exhibit)
     exhibits.push(exhibit)
   end
+
+  def recommend_exhibits(patron)
+    exhibit_recs = []
+    exhibits.each do |exhibit|
+      patron.interests.each do |interest|
+        if interest == exhibit.name
+          exhibit_recs << exhibit
+        end
+      end
+    end
+    exhibit_recs
+  end
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -30,23 +30,24 @@ class MuseumTest < Minitest::Test
 
     assert_equal [gems_and_minerals, dead_sea_scrolls, imax], @dmns.exhibits
   end
+
+  def test_it_can_recommend_exhibits
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @dmns.add_exhibit(gems_and_minerals)
+    @dmns.add_exhibit(dead_sea_scrolls)
+    @dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    assert_equal [gems_and_minerals, dead_sea_scrolls], @dmns.recommend_exhibits(patron_1)
+    assert_equal [imax], @dmns.recommend_exhibits(patron_2)
+  end
 end
-
-
-# pry(main)> patron_1 = Patron.new("Bob", 20)
-# # => #<Patron:0x00007fb400a51cc8...>
-#
-# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> patron_1.add_interest("Gems and Minerals")
-#
-# pry(main)> patron_2 = Patron.new("Sally", 20)
-# # => #<Patron:0x00007fb400036338...>
-#
-# pry(main)> patron_2.add_interest("IMAX")
-#
-# pry(main)> dmns.recommend_exhibits(patron_1)
-# # => [#<Exhibit:0x00007fb400bbcdd8...>, #<Exhibit:0x00007fb400b851f8...>]
-#
-# pry(main)> dmns.recommend_exhibits(patron_2)
-# # => [#<Exhibit:0x00007fb400acc590...>]
