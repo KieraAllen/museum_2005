4c8042c4419c749ab244992c35688d54a8488f1a
Kiera Allen <kieraaallen@gmail.com>
2020-05-27 10:01:56 -0600

    Add test_it_can_admit_patrons

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -62,6 +62,32 @@ class MuseumTest < Minitest::Test
 
     assert_equal [], @dmns.patrons
   end
+
+  def test_it_can_admit_patrons
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @dmns.add_exhibit(gems_and_minerals)
+    @dmns.add_exhibit(dead_sea_scrolls)
+    @dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 0)
+    patron_1.add_interest("Gems and Minerals")
+    patron_1.add_interest("Dead Sea Scrolls")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("Dead Sea Scrolls")
+
+    patron_3 = Patron.new("Johnny", 5)
+    patron_3.add_interest("Dead Sea Scrolls")
+
+    @dmns.admit(patron_1)
+    @dmns.admit(patron_2)
+    @dmns.admit(patron_3)
+
+    assert_equal [patron_1, patron_2, patron_3], @dmns.patrons
+  end
 end
 
 # - For `patrons_by_exhibit_interest`,
@@ -80,33 +106,6 @@ end
 # `announce_lottery_winner` method in conjunction
 # with the `draw_lottery_winner` method. JOY!
 
-# pry(main)> patron_1 = Patron.new("Bob", 0)
-# # => #<Patron:0x00007fb2011455b8...>
-#
-# pry(main)> patron_1.add_interest("Gems and Minerals")
-#
-# pry(main)>
-# patron_1.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> patron_2 = Patron.new("Sally", 20)
-# # => #<Patron:0x00007fb20227f8b0...>
-#
-# pry(main)> patron_2.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> patron_3 = Patron.new("Johnny", 5)
-# # => #<Patron:0x6666fb20114megan...>
-#
-# pry(main)> patron_3.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> dmns.admit(patron_1)
-#
-# pry(main)> dmns.admit(patron_2)
-#
-# pry(main)> dmns.admit(patron_3)
-#
-# pry(main)> dmns.patrons
-# # => [#<Patron:0x00007fb2011455b8...>, #<Patron:0x00007fb20227f8b0...>, #<Patron:0x6666fb20114megan...>]
-#
 # #Patrons are added even if they don't have enough money for all/any exhibits.
 #
 # pry(main)> dmns.patrons_by_exhibit_interest
